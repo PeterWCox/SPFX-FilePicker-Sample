58371c49575f80277f11b24f7f55c6383a00b868
Peter Cox <pcox@contentformula.com>
2022-11-15 01:20:55 +0000

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,17 @@
-'use strict';
-
-const build = require('@microsoft/sp-build-web');
-
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
+"use strict";
+const build = require("@microsoft/sp-build-web");
+build.addSuppression(/Warning - /gi);
+build.addSuppression(/Warning - lint -/gi); //uncomment for dev work
 var getTasks = build.rig.getTasks;
-build.rig.getTasks = function () {
+build.rig.getTasks = function ()
+{
   var result = getTasks.call(build.rig);
-
-  result.set('serve', result.get('serve-deprecated'));
-
+  result.set("serve", result.get("serve-deprecated"));
   return result;
 };
-
 /* fast-serve */
 const { addFastServe } = require("spfx-fast-serve-helpers");
 addFastServe(build);
 /* end of fast-serve */
-
-build.initialize(require('gulp'));
-
+build.initialize(require("gulp"));
+build.sass.setConfig({ warnOnNonCSSModules: false, useCssModules: true });
